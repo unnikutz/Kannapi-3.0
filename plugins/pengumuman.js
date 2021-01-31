24f3a5a777bbb35299aa0d81202aa519c4a304c9
unnikutz <78098073+unnikutz@users.noreply.github.com>
2021-02-01 00:44:20 +0530

    Update pengumuman.js

--- plugins/pengumuman.js
+++ plugins/pengumuman.js
@@ -4,16 +4,16 @@ let handler = async (m, { conn, text, participants }) => {
   let users = participants.map(u => u.jid)
   conn.reply(m.chat, text, m, { contextInfo: { mentionedJid: users } })
 }
-handler.help = ['pengumuman','announce','hidetag'].map(v => v + ' [teks]')
+handler.help = ['resend <text>','announce <text>','hidetag <text>'].map(v => v + ' [teks]')
 handler.tags = ['group']
-handler.command = /^(pengumuman|announce|hiddentag|hidetag)$/i
-handler.owner = false
+handler.command = /^(resend|announce|hiddentag|hidetag)$/i
+handler.owner = true
 handler.mods = false
 handler.premium = false
-handler.group = true
+handler.group = false
 handler.private = false
 
-handler.admin = true
+handler.admin = false
 handler.botAdmin = false
 
 handler.fail = null
