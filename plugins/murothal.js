c779705fa2fe9919499f4bd40f6c16a09487343e
unnikutz <78098073+unnikutz@users.noreply.github.com>
2021-02-01 00:49:39 +0530

    Update murothal.js

--- plugins/murothal.js
+++ plugins/murothal.js
@@ -1,18 +1,6 @@
 let handler  = async (m, { conn, usedPrefix: _p }) => {
   conn.reply(m.chat, `
-MR RAVANAN VIDEOS PLAYLIST:
-SUPPORT{RANJITH}
-
-*ʏᴏᴜᴛᴜʙᴇ:*
-_~https://youtube.com/c/MrRavanan_
-
-ᴛᴇʀᴍᴜx ɴᴇᴡ ᴛᴏᴏʟs ᴀɴᴅ sᴄʀɪᴘᴛ [ᴘʟᴀʏʟɪsᴛ]⇨ https://youtube.com/playlist?list=PLXI_OgWidZkWteWpz5wfttOd3lmYmHBCY
-ɪɴsᴛᴀɢʀᴀᴍ ғʀᴇᴇ ʙᴏᴛ sᴄʀɪᴘᴛ ⇨ https://youtu.be/cNCMXThkpd4
-ᴡʜᴀᴛs ᴀᴘᴘ ʙᴏᴛ sᴄʀɪᴘᴛ ⇨ https://youtu.be/tD_6JgA9Gmc
-ɪɴsᴛᴀɢʀᴀᴍ ғʀᴇᴇ ғᴏʟʟᴏᴡᴇʀs ᴠɪᴅᴇᴏs [ᴘʟᴀʏʟɪsᴛ] ⇨ https://youtube.com/playlist?list=PLXI_OgWidZkWjIXlfkQ8HpF8dUJ46vJza
-ʜᴏᴡ ᴛᴏ ɢᴇᴛ ғᴀᴋᴇ ᴡʜᴀᴛs ᴀᴘᴘ ɴᴜᴍʙᴇʀs [ᴘʟᴀʏʟɪsᴛ]⇨ https://youtube.com/playlist?list=PLXI_OgWidZkU35XPL64coDqNMEtktTJwR
-kali nethunter installation android ⇨ https://youtu.be/He5wqMb3E_g
-`.trim(), m)
+```Yet Not Available```
 }
 handler.help = ['murothal']
 handler.tags = ['quotes']
