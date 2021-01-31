7ffab0d43ff47e0af4587c0f424a125e05538865
unnikutz <78098073+unnikutz@users.noreply.github.com>
2021-02-01 01:07:33 +0530

    Update info.js

--- plugins/info.js
+++ plugins/info.js
@@ -2,27 +2,16 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
   conn.reply(m.chat, `
 ╠═〘 INFO BOT 〙 ═
 ╠➥ Made in javascript via NodeJs
-╠➥ Rec: ᴍʀ.ʀᴀᴠᴀɴᴀɴ [ ʀᴀɴᴊɪᴛʜ.ᴍ ]
-╠➥ Script: ᴍʀ ʀᴀᴠᴀɴᴀɴ ™
-║      [ᴅᴏɴᴛ ᴇᴅɪᴛ ᴄʀᴇᴅɪᴛs / ᴄʜᴇᴄᴋ  ʟɪᴄᴇɴsᴇ ]
-╠➥ ㄚㄖ凵ㄒ凵乃乇: https://youtube.com/c/MrRavanan
-╠➥ ㄚㄖ凵ㄒ凵乃乇 2几ᗪ 匚卄: https://youtube.com/channel/UChVky9HwaivYVdQRMCrM9Ig
-╠➥ 丨几丂ㄒ卂Ꮆ尺卂爪: https://instagram.com/__mr_ravanan?igshid=1lu9qhoz5czrn
+╠➥ Rec: Mr.*Abhinav* [ Abhinav ]
+╠➥ Script: Mr Abhinav ™
+║      [WhatsAlexa]
 ║
 ╠═〘 Thanks To 〙 ═
-╠➥ ɪɴᴅᴏɴᴇsɪᴀ ™
-╠➥ ᴍʀ ʀᴀᴠᴀɴᴀɴ ™
-╠➥ ᴍᴀʟᴀʏᴀʟɪᴢᴢ / ʏᴛ sᴜᴘᴘᴏʀᴛᴇʀs
+╠➥ WhatsAlexa ™
+╠➥ Mr. Abhinav™
 ╠➥ And other friends :)
 ║
-╠═〘 support ™ 〙 ═
-╠➥ https://youtube.com/c/MrRavanan
-╠➥ https://youtube.com/channel/UChVky9HwaivYVdQRMCrM9Ig
-╠➥ send message : wa.me//+919048410619
-║
-║>ɪғ ʏᴏɪ ʜᴀᴠᴇ ᴀɴʏ ɪssᴜᴇ sᴇɴᴅ ᴍᴇssᴀɢᴇ? wa.me//+919048410619
-║
-╠═〘 ʀ@ᴠ@ɴ@ɴ BOT 〙 ═
+╠═〘 WhatsAlexa BOT 〙 ═
 `.trim(), m)
 }
 handler.help = ['info']
@@ -41,3 +30,4 @@ handler.fail = null
 
 module.exports = handler
 
+ 
